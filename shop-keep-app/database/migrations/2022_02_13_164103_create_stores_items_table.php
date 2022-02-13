a3204441b53b609ad51359904c7f95ec94b42bc5
GBro <george@teemill.com>
2022-02-13 16:42:54 +0000

    migrate: create store items join table

---
+++ shop-keep-app/database/migrations/2022_02_13_164103_create_stores_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('store_id')->constrained();
+            $table->foreignId('item_id')->constrainted();
+            $table->integer('quantity')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('store_items');
+    }
+};
