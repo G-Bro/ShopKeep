0ff8f12437be36f3c1271fb7c270734cb6639dc9
GBro <george@teemill.com>
2022-02-13 16:40:21 +0000

    refactor: change store factory faker to use text rather than name

--- shop-keep-app/database/factories/StoreFactory.php
+++ shop-keep-app/database/factories/StoreFactory.php
@@ -17,7 +17,7 @@ class StoreFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->text(50),
             'description' => $this->faker->text(200),
         ];
     }
