155a6f0530573bc80abc92d51f7c33237d11f66d
GBro <george@teemill.com>
2022-02-13 15:38:08 +0000

    test: Inventory tests

---
+++ shop-keep-app/tests/Feature/InventoryTest.php
@@ -0,0 +1,103 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+uses(RefreshDatabase::class);
+
+beforeEach(function () { 
+    Store::factory()->create();
+    Item::factory()->create();
+});
+
+it('has store')->assertDatabaseHas('stores', [
+    'id' => 1,
+]);
+
+it('has items')->assertDatabaseHas('items', [
+    'id' => 1,
+]);
+
+it('can add item to a store', function () {
+    // arrange
+    $store = Store::find(1);
+    $item = Item::find(1);
+
+    $quantity = 5;
+
+    // act
+    $store->addInventory(
+        $item,
+        $quantity
+    );
+
+    // assert
+    expect($store->getInventory($item))
+        ->toBeInt()
+        ->toEqual($quantity);
+});
+
+it('can increase stock level of an item in a store', function () {
+    // arrange
+    $store = Store::find(1);
+    $item = Item::find(1);
+
+    $quantity = 5;
+
+    // act
+    // add the item to stock twice and confirm that the quantity goes up each time
+    $store->addInventory(
+        $item,
+        $quantity
+    );
+
+    $store->addInventory(
+        $item,
+        $quantity
+    );
+
+    // assert
+    expect($store->getInventory($item))
+        ->toBeInt()
+        ->toEqual($quantity * 2);
+});
+
+it('can decrease stock level of an item in a store', function () {
+    // arrange
+    $store = Store::find(1);
+    $item = Item::find(1);
+
+    $starting_quantity = 5;
+
+    // act
+    $store->addInventory(
+        $item,
+        $starting_quantity
+    );
+
+    $store->addInventory(
+        $item,
+        -1
+    );
+
+    // assert
+    expect($store->getInventory($item))
+        ->toBeInt()
+        ->toEqual($starting_quantity - 1);
+});
+
+it('can\'t take stock for an item that doesn\'t exist', function () {
+        // arrange
+        $store = Store::find(1);
+        $item = Item::find(1);
+    
+        $starting_quantity = 5;
+    
+        // act    
+        $store->addInventory(
+            $item,
+            -1
+        );
+    
+        // assert
+        expect($store->getInventory($item))
+            ->toThrow(InventoryException::class);
+});
